b504dabef3133787fa17af2aec73020cf3d19253
IgorEverton <seu-email@gmail.com>
2025-02-13 19:19:55 -0300

    adicionando projeto

---
+++ landing-page/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cabecalho from "./components/Cabecalho/index.jsx";
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+    </>
+  );
+}
+
+export default App;
---
+++ landing-page/src/components/Cabecalho/index.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Cabecalho() {
+  return (
+    <>
+      <h1>
+        Teste
+      </h1>
+    </>
+  );
+}
+
+export default Cabecalho;
